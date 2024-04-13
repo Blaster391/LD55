f61e315c4b9650eb34bda4cdce7dde87a3c94a11
Robert Arnold <robertarnold391@gmail.com>
2024-04-13 09:28:16 +0100

    Folder stuff

---
+++ LD55/Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float m_movementSpeed = 1.0f;
+
+    private Vector2 m_movementInput = Vector2.zero;
+
+
+    private void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+    private void Update()
+    {
+        m_movementInput = Vector2.zero;
+        if (Input.GetButton("Forwards"))
+        {
+
+        }
+
+        if (Input.GetButton("Forwards"))
+        {
+
+        }
+
+        if (Input.GetButton("Forwards"))
+        {
+
+        }
+
+        if (Input.GetButton("Forwards"))
+        {
+
+        }
+
+
+    }
+}

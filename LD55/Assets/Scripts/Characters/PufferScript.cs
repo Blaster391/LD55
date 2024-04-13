66d00be041ac8f9198013e16ab943cfd5d91e6c4
Robert Arnold <robertarnold391@gmail.com>
2024-04-13 19:31:04 +0100

    Make pufferfish puff

---
+++ LD55/Assets/Scripts/Characters/PufferScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PufferScript : MonoBehaviour
+{
+    [SerializeField]
+    private float m_maxPuff = 3.0f;
+    [SerializeField]
+    private float m_maxPuffTime = 10.0f;
+
+    private bool m_grow = true;
+    private float m_puffTime = 0.0f;
+    private Vector3 m_scaleRef = Vector3.zero;
+
+    void Start()
+    {
+        m_scaleRef = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_grow)
+        {
+            m_puffTime += Time.deltaTime;
+            if(m_puffTime > m_maxPuffTime)
+            {
+                m_grow = false;
+            }
+        }
+        else
+        {
+            m_puffTime -= Time.deltaTime;
+            if (m_puffTime < 0.0f)
+            {
+                m_grow = true;
+            }
+        }
+
+        float alpha = Mathf.Lerp(0.0f, 1.0f, m_puffTime / m_maxPuffTime);
+        transform.localScale = m_scaleRef * m_maxPuff * alpha;
+
+    }
+}

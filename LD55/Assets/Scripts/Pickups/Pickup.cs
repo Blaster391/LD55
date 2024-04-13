67f17b0ca0d305364f5066e222bfc1c543ebb144
Robert Arnold <robertarnold391@gmail.com>
2024-04-13 22:57:18 +0100

    Tweak drop effects

--- LD55/Assets/Scripts/Pickups/Pickup.cs
+++ LD55/Assets/Scripts/Pickups/Pickup.cs
@@ -16,6 +16,10 @@ public abstract class Pickup : MonoBehaviour
     bool m_initialBounce = true;
     [SerializeField]
     private float m_bounceTotalTime = 1.0f;
+    [SerializeField]
+    private float m_bounceMinTotalTime = 0.5f;
+    [SerializeField]
+    private float m_bounceMaxTotalTime = 1.0f;
 
     private bool m_bounceLeft = false;
     private float m_horizontalBounce = 1.0f;
@@ -29,6 +33,7 @@ public abstract class Pickup : MonoBehaviour
         m_bounceLeft = Random.value > 0.5f;
         m_horizontalBounce = Random.Range(m_minHorizontalBounce, m_maxHorizontalBounce);
         m_verticalBounce = Random.Range(m_minVerticalBounce, m_maxVerticalBounce);
+        m_bounceTotalTime = Random.Range(m_bounceMinTotalTime, m_bounceMaxTotalTime);
     }
 
     // Update is called once per frame
@@ -38,7 +43,7 @@ public abstract class Pickup : MonoBehaviour
         {
             m_bounceTime += Time.deltaTime;
 
-            float alpha = (m_bounceTime / m_bounceTotalTime);
+            float alpha = (m_bounceTime / m_bounceMaxTotalTime);
 
             Vector2 newPosition = m_initialPosition;
 

79668d4809bb06d360b5a04b2ec670a01f902fd2
Giles Penfold <gtpenfold@hotmail.co.uk>
2024-04-15 22:35:59 +0100

    Fix for build

--- LD55/Assets/Scripts/UIManager.cs
+++ LD55/Assets/Scripts/UIManager.cs
@@ -1,9 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
-using UnityEngine.UI;
-using Unity.VisualScripting;
 
 public class UIManager : MonoBehaviour
 {
